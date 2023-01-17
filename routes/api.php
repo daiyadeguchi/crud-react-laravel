f8c7f9fe7c83cc8f44a62c7a5f749dc73aa83594
daiyadeguchi <daiyadeguchi@gmail.com>
2023-01-17 10:28:54 +0900

    add react to the project

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('product', ProductController::class);
+Route::resource('products', ProductController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
